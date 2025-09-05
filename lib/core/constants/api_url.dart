8329eb5d7e09be86551b6f205c8a1a796293341a
didierboka <didierboka.developer@gmail.com>
2025-09-05 21:51:06 +0000

    chore: ajout de cinetpay key

--- lib/core/constants/api_url.dart
+++ lib/core/constants/api_url.dart
@@ -2,4 +2,7 @@ class ApiUrl {
   static const String prod = "https://opisms.net/opisms-ws/api/v1/user";
   static const String prodAgent = "https://opisms.net/opisms-aws";
   static const String prodOrange = "https://e-sante.ci/api/orange/ussd";
+
+  static const String cinetPayApiKey = "140147965056c4a8c4ea9a57.23977351";
+  static const String cinetPaySiteId = "613434";
 }
