79465175594ca7d2a24257ba23e5e59a5cb03cf8
didierboka <didierboka.developer@gmail.com>
2026-03-21 02:00:25 +0000

    fix(ui): reactivation controle carnet/abonnement dans famille

--- lib/features/famille/presentation/widgets/family_card.dart
+++ lib/features/famille/presentation/widgets/family_card.dart
@@ -2,12 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:opicare/core/helpers/ui_helpers.dart';
 import 'package:opicare/core/res/styles/colours.dart';
-import 'package:opicare/core/res/styles/text_style.dart';
 import 'package:opicare/features/famille/data/models/family_member.dart';
 
 import '../../../../core/helpers/subscription_helper.dart';
 import '../../../carnet_sante/presentation/pages/carnet_sante_screen.dart';
 import '../../../user/data/models/user_model.dart';
+
+UserModel _userModelFromFamilyMember(FamilyMember member) {
+  return UserModel(
+    id: member.id,
+    patID: member.id,
+    name: member.name,
+    surname: member.surname,
+    email: '',
+    phone: '',
+    sex: member.sex,
+    birthdate: member.birthdate,
+    carnetPhoto: '',
+    userPic: '',
+    dateAbon: member.subscriptionDate,
+    dateExpiration: SubscriptionHelper.convertDate(member.expirationDate),
+    abonnementLabel: member.formula,
+  );
+}
 
 class FamilyMemberCard extends StatelessWidget {
   final FamilyMember member;
@@ -33,25 +50,23 @@ class FamilyMemberCard extends StatelessWidget {
             Text('Expiaration: ${member.expirationDate}'),
           ],
         ),
-        trailing: SubscriptionHelper.isSubscriptionExpired(UserModel(id: member.id, dateExpiration: SubscriptionHelper.convertDate(member.expirationDate), patID: member.id, phone: '', abonnementLabel: member.formula, birthdate: member.birthdate, carnetPhoto: '', dateAbon: member.subscriptionDate, email: '', name: member.name, sex: member.sex, surname: '', userPic: ''))
+        trailing: SubscriptionHelper.isSubscriptionExpired(
+                _userModelFromFamilyMember(member))
             ? IconButton(
                 icon: Icon(Icons.warning, color: Colors.red),
                 onPressed: () => SubscriptionHelper.showSubscriptionExpiredDialog(context),
               )
             : null,
         onTap: () {
-          // goto carnet screen with path parameter
-
-          //  if (SubscriptionHelper.isSubscriptionExpired(UserModel(id: member.id, dateExpiration: SubscriptionHelper.convertDate(member.expirationDate), patID: member.id, phone: '', abonnementLabel: member.formula, birthdate: member.birthdate, carnetPhoto: '', dateAbon: member.subscriptionDate, email: '', name: member.name, sex: member.sex, surname: '', userPic: ''))) {
-          //    SubscriptionHelper.showSubscriptionExpiredDialog(context);
-          //    return ;
-          //  }
-
-          //  if (!SubscriptionHelper.canAccessCarnet(UserModel(id: member.id, dateExpiration: SubscriptionHelper.convertDate(member.expirationDate), patID: member.id, phone: '', abonnementLabel: member.formula, birthdate: member.birthdate, carnetPhoto: '', dateAbon: member.subscriptionDate, email: '', name: member.name, sex: member.sex, surname: '', userPic: ''))) {
-          //    SubscriptionHelper.showCarnetAccessDeniedDialog(context);
-          //    return;
-          //  }
-
+          final user = _userModelFromFamilyMember(member);
+          if (SubscriptionHelper.isSubscriptionExpired(user)) {
+            SubscriptionHelper.showSubscriptionExpiredDialog(context);
+            return;
+          }
+          if (!SubscriptionHelper.canAccessCarnet(user)) {
+            SubscriptionHelper.showCarnetAccessDeniedDialog(context);
+            return;
+          }
           context.push('${CarnetSanteScreen.path}/${member.id}');
         },
       ),
